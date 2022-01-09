f9a16b48f2f27f6bd679aae3dea76fe6ab6ed4e2
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2022-01-09 19:33:45 +0100

    feat: refactor to allow default output

--- hello.go
+++ hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if len(name) > 0 {
+		return englishHelloPrefix + name
+	}
+	return englishHelloPrefix + "World"
 }
 
 func main() {
--- hello_again_test.go
+++ hello_again_test.go
@@ -3,10 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Niko")
-	want := "Hello, Niko"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Niko")
+		want := "Hello, Niko"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("say 'Hello, world' when empty string supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
