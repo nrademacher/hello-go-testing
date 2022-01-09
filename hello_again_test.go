1ae0f116f697b71d9b60999f7b9bb24c0b74e260
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2022-01-09 19:37:47 +0100

    chore: refactor tests with helper

--- hello_again_test.go
+++ hello_again_test.go
@@ -3,20 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Niko")
-		want := "Hello, Niko"
-
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper() // required to tell test suite func is a helper
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Niko")
+		want := "Hello, Niko"
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, world' when empty string supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
